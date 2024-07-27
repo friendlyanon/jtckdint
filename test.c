90cf1f2ed2d4ee5a7a6fb8b1f7a46a21fdf14b9c
friendlyanon <friendlyanon@users.noreply.github.com>
2024-07-27 19:27:41 +0200

    Make `test.c` work for non-GCC compilers
    Testing required the presence of "__builtin_{add,sub,mul}_overflow"
    before.

--- test.c
+++ test.c
@@ -67,6 +67,10 @@ typedef unsigned __int128 uint128_t;
       (T)(TMAX(T) / 2 - 4), \
   }
 
+#ifdef _MSC_VER
+#  pragma warning(push)
+#  pragma warning(disable : 4293)
+#endif
 DECLARE_TEST_VECTORS(int8_t);
 DECLARE_TEST_VECTORS(uint8_t);
 DECLARE_TEST_VECTORS(int16_t);
@@ -79,104 +83,142 @@ DECLARE_TEST_VECTORS(uint64_t);
 DECLARE_TEST_VECTORS(int128_t);
 DECLARE_TEST_VECTORS(uint128_t);
 #endif
+#ifdef _MSC_VER
+#  pragma warning(pop)
+#endif
 
 static FILE* reference;
-
-#define print(T, I, o, x) \
-  do { \
-    T v = (x); \
-    int count = sizeof(T); \
-    unsigned char buffer[17]; \
-    int index = sizeof(buffer); \
-    while (1) { \
-      buffer[--index] = (unsigned char)(v & 0xFF); \
-      if (--count == 0) { \
-        break; \
-      } \
-      v >>= 8; \
-    } \
-    buffer[--index] = (unsigned char)(((I) << 7) | ((o) << 6) | sizeof(T)); \
-    assert(fwrite(buffer + index, 1, sizeof(buffer) - index, reference) \
-           == sizeof(buffer) - index); \
-  } while (0)
 
 int main(int argc, char const* argv[])
 {
   (void)argc;
   (void)argv;
 
-  reference = fopen("test.bin", "wb");
+  reference = fopen("test.bin", "rb");
   assert(reference);
 
-#define M(T, U, V, I) \
+#ifdef ckd_have_int128
+#  define read() \
+    do { \
+      assert(fread(&ref, 1, 1, reference) == 1); \
+      size = ref & 0x3F; \
+      assert(fread(&buffer, 1, size, reference) == size); \
+    } while (0)
+#else
+#  define read() \
+    do { \
+      assert(fread(&ref, 1, 1, reference) == 1); \
+      size = ref & 0x3F; \
+      if ((ref & 0x80) == 0) { \
+        assert(fread(&buffer, 1, size, reference) == size); \
+        break; \
+      } \
+      assert(!fseek(reference, (long)size, SEEK_CUR)); \
+      offset += 1 + (long)size; \
+    } while (1)
+#endif
+
+#define stringify(N) \
+  do { \
+    int s = z##N < 0 ? -1 : 1; \
+    i##N = sizeof(c##N); \
+    c##N[--i##N] = 0; \
+    do { \
+      c##N[--i##N] = '0' + (char)(s * (z##N % 10)); \
+      z##N /= 10; \
+    } while (z##N != 0); \
+    if (s == -1) { \
+      c##N[--i##N] = '-'; \
+    } \
+  } while (0)
+
+#define check_next(expr) \
+  do { \
+    int o1, o2; \
+    size_t size = 0, index = 0; \
+    unsigned char buffer[16]; \
+    unsigned char ref = 0; \
+    long offset = ftell(reference); \
+    read(); \
+    z2 = buffer[index++]; \
+    while (index != size) { \
+      z2 = (z2 << 8) | buffer[index++]; \
+    } \
+    o1 = !!(expr); \
+    o2 = (ref & 0x40) != 0; \
+    if (o1 != o2 || z1 != z2) { \
+      char c1[50], c2[50]; \
+      int i1, i2; \
+      stringify(1); \
+      stringify(2); \
+      assert(!(fprintf(stderr, \
+                       "Mismatch @ 0x%lX: (%c) %s != (%c) %s\n", \
+                       offset, \
+                       '0' + o1, \
+                       c1 + i1, \
+                       '0' + o2, \
+                       c2 + i2) \
+               < 0)); \
+      return 1; \
+    } \
+  } while (0)
+
+#define M(T, U, V) \
   for (int i = 0; i != (int)(sizeof(k##U) / sizeof(k##U[0])); ++i) { \
     U x = k##U[i]; \
     for (int j = 0; j != (int)(sizeof(k##V) / sizeof(k##V[0])); ++j) { \
-      int o1, o2; \
       T z1, z2; \
       V y = k##V[j]; \
-      o1 = ckd_add(&z1, x, y); \
-      o2 = __builtin_add_overflow(x, y, &z2); \
-      assert(o1 == o2); \
-      assert(z1 == z2); \
-      print(T, (I), o2, z2); \
-      o1 = ckd_sub(&z1, x, y); \
-      o2 = __builtin_sub_overflow(x, y, &z2); \
-      assert(o1 == o2); \
-      assert(z1 == z2); \
-      print(T, (I), o2, z2); \
-      o1 = ckd_mul(&z1, x, y); \
-      o2 = __builtin_mul_overflow(x, y, &z2); \
-      assert(o1 == o2); \
-      assert(z1 == z2); \
-      print(T, (I), o2, z2); \
+      check_next(ckd_add(&z1, x, y)); \
+      check_next(ckd_sub(&z1, x, y)); \
+      check_next(ckd_mul(&z1, x, y)); \
     } \
   }
 
 #ifdef ckd_have_int128
-#  define MM(T, U, I) \
-    M(T, U, uint8_t, 0 | (I)) \
-    M(T, U, uint16_t, 0 | (I)) \
-    M(T, U, uint32_t, 0 | (I)) \
-    M(T, U, uint64_t, 0 | (I)) \
-    M(T, U, uint128_t, 1 | (I)) \
-    M(T, U, int8_t, 0 | (I)) \
-    M(T, U, int16_t, 0 | (I)) \
-    M(T, U, int32_t, 0 | (I)) \
-    M(T, U, int64_t, 0 | (I)) \
-    M(T, U, int128_t, 1 | (I))
-#  define MMM(T, I) \
-    MM(T, uint8_t, 0 | (I)) \
-    MM(T, uint16_t, 0 | (I)) \
-    MM(T, uint32_t, 0 | (I)) \
-    MM(T, uint64_t, 0 | (I)) \
-    MM(T, uint128_t, 1 | (I)) \
-    MM(T, int8_t, 0 | (I)) \
-    MM(T, int16_t, 0 | (I)) \
-    MM(T, int32_t, 0 | (I)) \
-    MM(T, int64_t, 0 | (I)) \
-    MM(T, int128_t, 1 | (I))
-  MMM(uint8_t, 0)
-  MMM(uint16_t, 0)
-  MMM(uint32_t, 0)
-  MMM(uint64_t, 0)
-  MMM(uint128_t, 1)
-  MMM(int8_t, 0)
-  MMM(int16_t, 0)
-  MMM(int32_t, 0)
-  MMM(int64_t, 0)
-  MMM(int128_t, 1)
+#  define MM(T, U) \
+    M(T, U, uint8_t) \
+    M(T, U, uint16_t) \
+    M(T, U, uint32_t) \
+    M(T, U, uint64_t) \
+    M(T, U, uint128_t) \
+    M(T, U, int8_t) \
+    M(T, U, int16_t) \
+    M(T, U, int32_t) \
+    M(T, U, int64_t) \
+    M(T, U, int128_t)
+#  define MMM(T) \
+    MM(T, uint8_t) \
+    MM(T, uint16_t) \
+    MM(T, uint32_t) \
+    MM(T, uint64_t) \
+    MM(T, uint128_t) \
+    MM(T, int8_t) \
+    MM(T, int16_t) \
+    MM(T, int32_t) \
+    MM(T, int64_t) \
+    MM(T, int128_t)
+  MMM(uint8_t)
+  MMM(uint16_t)
+  MMM(uint32_t)
+  MMM(uint64_t)
+  MMM(uint128_t)
+  MMM(int8_t)
+  MMM(int16_t)
+  MMM(int32_t)
+  MMM(int64_t)
+  MMM(int128_t)
 
 #else
 #  define MM(T, U) \
-    M(T, U, uint8_t, 0) \
-    M(T, U, uint16_t, 0) \
-    M(T, U, uint32_t, 0) \
-    M(T, U, uint64_t, 0) \
-    M(T, U, int8_t, 0) \
-    M(T, U, int16_t, 0) \
-    M(T, U, int32_t, 0) \
-    M(T, U, int64_t, 0)
+    M(T, U, uint8_t) \
+    M(T, U, uint16_t) \
+    M(T, U, uint32_t) \
+    M(T, U, uint64_t) \
+    M(T, U, int8_t) \
+    M(T, U, int16_t) \
+    M(T, U, int32_t) \
+    M(T, U, int64_t)
 #  define MMM(T) \
     MM(T, uint8_t) \
     MM(T, uint16_t) \
@@ -194,7 +236,29 @@ int main(int argc, char const* argv[])
   MMM(int16_t)
   MMM(int32_t)
   MMM(int64_t)
+  while (1) {
+    unsigned char ref = 0;
+    if (fread(&ref, 1, 1, reference) != 1) {
+      break;
+    }
+    assert(ref & 0x80);
+    assert(!fseek(reference, ref & 0x3F, SEEK_CUR));
+  }
 #endif
 
+  if (fgetc(reference) != EOF || !feof(reference)) {
+    long current = ftell(reference);
+    long end = 0;
+    assert(!fseek(reference, 0, SEEK_END));
+    end = ftell(reference);
+    assert(!(fprintf(stderr,
+                     "Reference was not read to completion. "
+                     "%ld bytes left.\n",
+                     end - current)
+             < 0));
+    return 1;
+  }
+
   assert(fclose(reference) == 0);
+  return 0;
 }
