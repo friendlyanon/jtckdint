75526196c55575443c63263af3cbe204d8fa8dc8
friendlyanon <friendlyanon@users.noreply.github.com>
2024-11-20 10:50:26 +0100

    Simplify error return checking in `test.bat`

--- test.bat
+++ test.bat
@@ -2,19 +2,13 @@
 
 if not "%jtckdint_build_mode%" == "" goto :build_with_mode
 
-call :self mingw32
-if not %errorlevel% == 0 exit /b %errorlevel%
-
-call :self mingw64
-if not %errorlevel% == 0 exit /b %errorlevel%
-
-call :self llvm
-if not %errorlevel% == 0 exit /b %errorlevel%
-
-call :self msvc32
-if not %errorlevel% == 0 exit /b %errorlevel%
-
+call :self mingw32 || goto :exit
+call :self mingw64 || goto :exit
+call :self llvm || goto :exit
+call :self msvc32 || goto :exit
 call :self msvc64
+
+:exit
 exit /b %errorlevel%
 
 :build_with_mode
@@ -49,33 +43,24 @@ setlocal
 
 echo ? arch=%arch%
 
-call mingw.bat %arch%
-if not %errorlevel% == 0 exit /b %errorlevel%
+call mingw.bat %arch% || goto :exit
 
 set comp=gcc.exe -isystem . -Wall -Wextra -Wconversion -Werror -o test.exe
 
-call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=gnu11
-if not %errorlevel% == 0 exit /b %errorlevel%
-call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -Wpedantic -std=c11
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=gnu11 || goto :exit
+call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -Wpedantic -std=c11 || goto :exit
 
-call :build -O0 -std=gnu11
-if not %errorlevel% == 0 exit /b %errorlevel%
-call :build -O0 -Wpedantic -std=c11
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -O0 -std=gnu11 || goto :exit
+call :build -O0 -Wpedantic -std=c11 || goto :exit
 
-call :build -O3 -std=gnu11
-if not %errorlevel% == 0 exit /b %errorlevel%
-call :build -O3 -Wpedantic -std=c11
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -O3 -std=gnu11 || goto :exit
+call :build -O3 -Wpedantic -std=c11 || goto :exit
 
 set comp=g++.exe -isystem . -Wall -Wextra -Wconversion -Wpedantic -Werror -o test.exe -x c++
 
-call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=c++14
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=c++14 || goto :exit
 
-call :build -O0 -std=c++14
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -O0 -std=c++14 || goto :exit
 
 call :build -O3 -std=c++14
 
@@ -88,28 +73,20 @@ if "%CLANG_VERSION_PREFIX%" == "" set CLANG_VERSION_PREFIX=C:\Program Files\LLVM
 
 set comp=clang.exe -isystem . -Weverything -Wno-declaration-after-statement -Wno-unsafe-buffer-usage -Wno-pre-c11-compat -Werror -D_CRT_SECURE_NO_WARNINGS=1 -o test.exe
 
-call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=gnu11 -l "%CLANG_VERSION_PREFIX%\lib\windows\clang_rt.builtins-x86_64.lib"
-if not %errorlevel% == 0 exit /b %errorlevel%
-call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=c11 -D__STRICT_ANSI__=1
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=gnu11 -l "%CLANG_VERSION_PREFIX%\lib\windows\clang_rt.builtins-x86_64.lib" || goto :exit
+call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=c11 -D__STRICT_ANSI__=1 || goto :exit
 
-call :build -O0 -std=gnu11 -l "%CLANG_VERSION_PREFIX%\lib\windows\clang_rt.builtins-x86_64.lib"
-if not %errorlevel% == 0 exit /b %errorlevel%
-call :build -O0 -std=c11 -D__STRICT_ANSI__=1
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -O0 -std=gnu11 -l "%CLANG_VERSION_PREFIX%\lib\windows\clang_rt.builtins-x86_64.lib" || goto :exit
+call :build -O0 -std=c11 -D__STRICT_ANSI__=1 || goto :exit
 
-call :build -O3 -std=gnu11 -l "%CLANG_VERSION_PREFIX%\lib\windows\clang_rt.builtins-x86_64.lib"
-if not %errorlevel% == 0 exit /b %errorlevel%
-call :build -O3 -std=c11 -D__STRICT_ANSI__=1
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -O3 -std=gnu11 -l "%CLANG_VERSION_PREFIX%\lib\windows\clang_rt.builtins-x86_64.lib" || goto :exit
+call :build -O3 -std=c11 -D__STRICT_ANSI__=1 || goto :exit
 
 set comp=clang++.exe -isystem . -Weverything -Wno-unsafe-buffer-usage -Wno-c++98-compat -Wno-c++98-compat-pedantic -Werror -D_CRT_SECURE_NO_WARNINGS=1 -o test.exe -x c++
 
-call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=c++14 -D__STRICT_ANSI__=1
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -Os -fsanitize=undefined -fsanitize-undefined-trap-on-error -std=c++14 -D__STRICT_ANSI__=1 || goto :exit
 
-call :build -O0 -std=c++14 -D__STRICT_ANSI__=1
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build -O0 -std=c++14 -D__STRICT_ANSI__=1 || goto :exit
 
 call :build -O3 -std=c++14 -D__STRICT_ANSI__=1
 
@@ -132,21 +109,17 @@ setlocal
 
 echo ? arch=%arch%
 
-call vcvars.bat -arch=%arch% -host_arch=amd64 -no_logo
-if not %errorlevel% == 0 exit /b %errorlevel%
+call vcvars.bat -arch=%arch% -host_arch=amd64 -no_logo || goto :exit
 
 set comp=cl.exe /nologo /Wall /wd4710 /WX /D_CRT_SECURE_NO_WARNINGS=1 /diagnostics:caret /external:I . /external:W0 /permissive- /Zc:inline /Zc:preprocessor
 
-call :build /Od /d2Obforceinline /std:c11
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build /Od /d2Obforceinline /std:c11 || goto :exit
 
-call :build /O2 /wd4711 /wd4883 /std:c11
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build /O2 /wd4711 /wd4883 /std:c11 || goto :exit
 
 set comp=%comp% /EHsc /TP /Zc:__cplusplus
 
-call :build /Od /d2Obforceinline
-if not %errorlevel% == 0 exit /b %errorlevel%
+call :build /Od /d2Obforceinline || goto :exit
 
 call :build /O2 /wd4711 /wd4883
 
@@ -154,8 +127,7 @@ exit /b %errorlevel%
 
 :build
 echo ^< %comp% %* test.c other.c
-%comp% %* test.c other.c
-if not %errorlevel% == 0 exit /b %errorlevel%
+%comp% %* test.c other.c || goto :exit
 
 echo ^> test.exe
 test.exe
