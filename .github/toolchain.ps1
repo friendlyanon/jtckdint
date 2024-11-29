1a3515171f8c16279fdbe2fe5cfcff4fc3ddcd30
friendlyanon <friendlyanon@users.noreply.github.com>
2024-11-29 03:57:45 +0100

    Disable chocolatey download progress

--- .github/toolchain.ps1
+++ .github/toolchain.ps1
@@ -7,7 +7,7 @@ if ($compiler -eq 'mingw') {
   Start-Process "mingw-$arch.exe" -ArgumentList '-y', '-o.' -Wait
   Set-Content mingw.bat "@echo off`r`nset CC=gcc.exe`r`nset CXX=g++.exe`r`nset `"PATH=%PATH%;%cd%\w64devkit\bin`""
 } elseif ($compiler -eq 'llvm') {
-  choco.exe install llvm --version=19.1.3
+  choco.exe install llvm --version=19.1.3 --no-progress
 } else {
   Set-Content vcvars.bat "@echo off`r`nfor /F `"delims=`" %%g in ('`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe`" -Property InstallationPath') do set dir=%%g`r`ncall `"%dir%\Common7\Tools\vsdevcmd.bat`" -arch=$arch -host_arch=amd64 -no_logo"
 }
